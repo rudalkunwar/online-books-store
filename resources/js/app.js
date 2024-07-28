c6586c6fdbb4a3b0d402e54502859eb8a63646e8
rudal kunwar <ruzalkunwar@gmail.com>
2024-07-28 22:09:32 +0545

    landing page with header and footer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,8 @@
 import './bootstrap';
-
 import Alpine from 'alpinejs';
+import Toastify from 'toastify-js';
+import 'toastify-js/src/toastify.css';
+window.Toastify = Toastify; // Make Toastify available globally
 
 window.Alpine = Alpine;
-
-Alpine.start();
+Alpine.start ();
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -180,7 +180,27 @@
             </div>
         </div>
     </div>
-
+    
+    @php
+    session_start();
+    @endphp
+    
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const sessionMessage = '<?php echo session('success') ? session('success') : ''; ?>';
+            if (sessionMessage) {
+                Toastify({
+                    text: sessionMessage,
+                    duration: 4000,
+                    close: true,
+                    gravity: "top",
+                    position: "right",
+                    backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+                    stopOnFocus: true,
+                }).showToast();
+            }
+        });
+    </script>
 </body>
 
 </html>
